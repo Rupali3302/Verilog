5a9ddf5605d80162605a1efbf1c469d36e8eca8e
Rupali3302 <rupalitirumalasetty@gmail.com>
2025-05-13 13:10:53 +0530

    half_adder.v

--- half_adder/half_adder.v
+++ half_adder/half_adder.v
@@ -1,7 +1,32 @@
+
+//Gate level
+
 module half_adder(a,b,sum,carry);
     input a,b;
     output sum,carry;
     wire sum,carry;
     xor x1(sum,a,b);
     and a1(carry,a,b);
+endmodule
+
+// Data flow model
+
+module half_adder(a,b,sum,carry);
+    input a,b;
+    output sum,carry;
+    assign sum=a^b;
+    assign carry=a&b;
+endmodule
+
+//Behavioural model
+
+
+module half_adder(a,b,sum,carry);
+    input a,b;
+    output reg sum,carry;
+    always @(a,b)
+    begin 
+    	sum=a^b;
+    	carry=a&b;
+    end
 endmodule
