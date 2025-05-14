d4d21415339d14acbdeea5eba31bc130b48c6ac0
Rupali3302 <rupalitirumalasetty@gmail.com>
2025-05-14 17:14:48 +0530

    mux_4_1.v

--- mux_4_1/mux_4_1.v
+++ mux_4_1/mux_4_1.v
@@ -18,3 +18,9 @@ endmodule
 module mux_4_1(input [3:0]in,input [1:0]sel,output out);
   assign out=((~sel[1] & ~sel[0] & in[0])| (~sel[1] & sel[0] & in[1])|(sel[1] & ~sel[0] & in[2])| (sel[1] & sel[0] & in[3]));
 endmodule
+
+
+//Using Ternary operator
+module mux_4_1(input [3:0]in,input [1:0]sel,output out);
+    assign out = (sel == 2'b00) ? in[0] :(sel == 2'b01) ? in[1] :(sel == 2'b10) ? in[2] :in[3];
+endmodule
